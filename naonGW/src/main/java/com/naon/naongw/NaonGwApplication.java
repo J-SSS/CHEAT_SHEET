585e84d2d260d43cdcdc7b6fd02c0165c8231815
Dongil_Jang <dongr5102@naver.com>
2023-11-15 13:23:24 +0900

    [일반] 레포지토리세팅

---
+++ naonGW/src/main/java/com/naon/naongw/NaonGwApplication.java
@@ -0,0 +1,13 @@
+package com.naon.naongw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaonGwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaonGwApplication.class, args);
+    }
+
+}
---
+++ naonGW/src/test/java/com/naon/naongw/NaonGwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naon.naongw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaonGwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
